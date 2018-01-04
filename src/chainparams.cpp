de1a46ad27d5d96e743ff58bdf335fd6c70c96e6
The Cuckoocoin Developers <cuckoocoin@outlook.com>
2018-01-04 16:16:01 +0100

    Bitcoin 0.12 chainparams cleanups

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -18,6 +18,44 @@ using namespace std;
 
 #include "chainparamsseeds.h"
 
+static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
+{
+    CMutableTransaction txNew;
+    txNew.nVersion = 1;
+    txNew.vin.resize(1);
+    txNew.vout.resize(1);
+    txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << std::vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+    txNew.vout[0].nValue = genesisReward;
+    txNew.vout[0].scriptPubKey = genesisOutputScript;
+
+    CBlock genesis;
+    genesis.nTime    = nTime;
+    genesis.nBits    = nBits;
+    genesis.nNonce   = nNonce;
+    genesis.nVersion = nVersion;
+
+    genesis.vtx.push_back(txNew);
+    genesis.hashPrevBlock.SetNull();
+    genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+
+    return genesis;
+}
+
+/**
+ * Build the genesis block. Note that the output of its generation
+ * transaction cannot be spent since it did not originally exist in the
+ * database.
+ *
+ * >>> from pyblake2 import blake2s
+ * >>> 'Cuckoocoin' + blake2s(b'NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56 BTC#433272 000000000000000002c46a68972be8e6f671774e773449ea81c198243e3782c8 BTC#500819 0000000000000000005087254f1c1aea6f75ff0735b440432fd8a68d56ae0bba DJIA close on 29 Nov 2017: 23,940.68').hexdigest()
+ */
+static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
+{
+    const char* pszTimestamp = "Cuckoocoine1a7a03cf54db76a3670bda29861f9ddb8337a194bd5c262b4e9d40116bbef9c";
+    const CScript genesisOutputScript = CScript() << ParseHex("049c135ebba6ab7549a4621aa09e9b63c1485f0bf180690f1dbd2bb4736a5a2496d2d96cb1d026f513f6a7e6beb918132b7bf75b7625c62f2185cbc0aa131b1540") << OP_CHECKSIG;
+    return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
+}
+
 /**
  * Main network
  */
@@ -41,6 +79,7 @@ public:
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 10 * 60;
         consensus.fPowAllowMinDifficultyBlocks = false;
+
         /** 
          * The message start string is designed to be unlikely to occur in normal data.
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
@@ -51,34 +90,13 @@ public:
         pchMessageStart[2] = 0xd8;
         pchMessageStart[3] = 0xcf;
         vAlertPubKey = ParseHex("0418a597ea5fbc28a6c0803194701c7764aadaea40d82202a14f30e681cea576cdd0e6bc7a351d3fa6035823cb488c39aa1da7d2943223a6b07e9f879bf5857a2b");
+
         nDefaultPort = 27333;
         nMinerThreads = 0;
         nMaxTipAge = 24 * 60 * 60;
         nPruneAfterHeight = 100000;
 
-        /**
-         * Build the genesis block. Note that the output of its generation
-         * transaction cannot be spent since it did not originally exist in the
-         * database.
-         *
-	 * >>> from pyblake2 import blake2s
-         * >>> 'Cuckoocoin' + blake2s(b'NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56 BTC#433272 000000000000000002c46a68972be8e6f671774e773449ea81c198243e3782c8 BTC#500819 0000000000000000005087254f1c1aea6f75ff0735b440432fd8a68d56ae0bba DJIA close on 29 Nov 2017: 23,940.68').hexdigest()
-         */
-        const char* pszTimestamp = "Cuckoocoine1a7a03cf54db76a3670bda29861f9ddb8337a194bd5c262b4e9d40116bbef9c";
-        CMutableTransaction txNew;
-        txNew.nVersion = 1;
-        txNew.vin.resize(1);
-        txNew.vout.resize(1);
-        txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
-        txNew.vout[0].nValue = 50 * COIN;
-        txNew.vout[0].scriptPubKey = CScript() << ParseHex("049c135ebba6ab7549a4621aa09e9b63c1485f0bf180690f1dbd2bb4736a5a2496d2d96cb1d026f513f6a7e6beb918132b7bf75b7625c62f2185cbc0aa131b1540") << OP_CHECKSIG;
-        genesis.vtx.push_back(txNew);
-        genesis.hashPrevBlock.SetNull();
-        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
-        genesis.nVersion = 1;
-        genesis.nTime    = 1231006505;
-        genesis.nBits    = 0x1d00ffff;
-        genesis.nNonce   = 2083236893;
+        genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
 
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
@@ -123,29 +141,40 @@ static CMainParams mainParams;
 /**
  * Testnet (v3)
  */
-class CTestNetParams : public CMainParams {
+class CTestNetParams : public CChainParams {
 public:
     CTestNetParams() {
         strNetworkID = "test";
+        consensus.nSubsidyHalvingInterval = 210000;
         consensus.nMajorityEnforceBlockUpgrade = 51;
         consensus.nMajorityRejectBlockOutdated = 75;
         consensus.nMajorityWindow = 100;
+        consensus.powLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
+        consensus.nPowTargetSpacing = 10 * 60;
         consensus.fPowAllowMinDifficultyBlocks = true;
+
+        /** 
+         * The message start string is designed to be unlikely to occur in normal data.
+         * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+         * a large 32-bit integer with any alignment.
+         */
         pchMessageStart[0] = 0x86;
         pchMessageStart[1] = 0x5d;
         pchMessageStart[2] = 0xfe;
         pchMessageStart[3] = 0x90;
         vAlertPubKey = ParseHex("04e11dc7b71501785424a6bafcca16eb2b975138c7434e46bbb9f002c4f23d7a82791f599cfe79228c7c969d1d5c85153e18a7a81f1fa98648e6db0feb89607f22");
+
         nDefaultPort = 37333;
         nMinerThreads = 0;
         nMaxTipAge = 0x7fffffff;
         nPruneAfterHeight = 1000;
 
-        //! Modify the testnet genesis block so the timestamp is valid for a later start.
-        genesis.nTime = 1296688602;
-        genesis.nNonce = 414098458;
+        genesis = CreateGenesisBlock(1296688602, 414098458, 0x1d00ffff, 1, 50 * COIN);
+
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
+        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
@@ -185,7 +214,7 @@ static CTestNetParams testNetParams;
 /**
  * Regression test
  */
-class CRegTestParams : public CTestNetParams {
+class CRegTestParams : public CChainParams {
 public:
     CRegTestParams() {
         strNetworkID = "regtest";
@@ -194,19 +223,35 @@ public:
         consensus.nMajorityRejectBlockOutdated = 950;
         consensus.nMajorityWindow = 1000;
         consensus.powLimit = uint256S("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
+        consensus.nPowTargetSpacing = 10 * 60;
+        consensus.fPowAllowMinDifficultyBlocks = true;
+
+        /** 
+         * The message start string is designed to be unlikely to occur in normal data.
+         * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+         * a large 32-bit integer with any alignment.
+         */
         pchMessageStart[0] = 0x71;
         pchMessageStart[1] = 0x19;
         pchMessageStart[2] = 0xea;
         pchMessageStart[3] = 0x8c;
+        vAlertPubKey = ParseHex("04e11dc7b71501785424a6bafcca16eb2b975138c7434e46bbb9f002c4f23d7a82791f599cfe79228c7c969d1d5c85153e18a7a81f1fa98648e6db0feb89607f22");
+
+        nDefaultPort = 47333;
         nMinerThreads = 1;
         nMaxTipAge = 24 * 60 * 60;
+        nPruneAfterHeight = 1000;
+
         genesis.nTime = 1296688602;
         genesis.nBits = 0x207fffff;
         genesis.nNonce = 2;
+
+        genesis = CreateGenesisBlock(1296688602, 2, 0x207fffff, 1, 50 * COIN);
+
         consensus.hashGenesisBlock = genesis.GetHash();
-        nDefaultPort = 47333;
         assert(consensus.hashGenesisBlock == uint256S("0x0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"));
-        nPruneAfterHeight = 1000;
+        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
         vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();  //! Regtest mode doesn't have any DNS seeds.
